a8a7343756d9c9d2c05d16a1868ec957f13476d0
vinocturne <vinocturne@naver.com>
2021-03-09 20:47:14 +0900

    2021.03.09 MemberApp 수정

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -14,9 +14,12 @@ public class MemberApp {
 
         //ApplicationContesxt, 스프링 컨테이너.
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //applicationContext가 AppConfig를 연결시켜주는 스프링 컨테이너로 작용.
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        //memberService를 생성하고, applicationContext로 AppConfig 클래스의 memberService를 가져올 수 있도록 설정.
 
         Member member = new Member(1L, "memberA", Grade.VIP);
+        //해당 memberService는 appConfig를 거쳐 생성된 memberService.
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
