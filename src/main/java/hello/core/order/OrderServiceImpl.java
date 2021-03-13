fd26432ea036454aa028434cd0dfa18064c475cd
vinocturne <vinocturne@naver.com>
2021-03-13 20:37:50 +0900

    2021.03.13 의존성 주입 관련

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,6 +12,7 @@ public class OrderServiceImpl implements OrderService{
     private final DiscountPolicy discountPolicy;
 
     @Autowired //생성자에 Autowired 어노테이션을 붙이면 자동으로 의존관계 주입.
+    //생성자가 하나일 때에는 Autowired가 없더라도 자동 의존성 주입
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,10 +1,8 @@
 package hello.core.order;
 
 import hello.core.AppConfig;
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,4 +27,12 @@ public class OrderServiceTest {
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
+
+ /*   @Test
+    void fieldInjectionTest() { //필드 인젝션도 가능은하지만 수정이 번거롭고 테스트가 까다롭기 때문에 잘 사용하지 않음
+        OrderServiceImpl orderService = new OrderServiceImpl();
+        orderService.setMemberRepository(new MemoryMemberRepository());
+        orderService.setDiscountPolicy(new FixDiscountPolicy());
+        orderService.createOrder(1L,"itemA",10000);
+    }*/
 }
